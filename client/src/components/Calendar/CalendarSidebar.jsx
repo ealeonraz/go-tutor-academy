f96fbd7feb859ba8fc562520dba50cbcecbe9099
Zack-Patty <zackpatty@csus.edu>
2025-05-02 12:38:56 -0700

    merge with team

--- client/src/components/Calendar/CalendarSidebar.jsx
+++ client/src/components/Calendar/CalendarSidebar.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { FaCalendarAlt, FaEdit, FaTrashAlt, FaStickyNote } from 'react-icons/fa';
-import AppointmentForm from '../Modals/CreateAppointmentModal.jsx';
-import Feedback from '../Feedback/Feedback.jsx';
-import './Calendar.css';
+import AppointmentForm from './CreateAppointmentModal.jsx';
+import Feedback from './Feedback.jsx';
+import './CalendarSidebar.css';
+import './calendar.css'
+import { useAuth } from '../context/AuthContext.jsx';
 
 export default function CalendarSidebar({
   events = [],
